1abb2cc4e7159bc010acd0b56d5d36f28806cec3
OlegNapapalam <69363909+OlegNapapalam@users.noreply.github.com>
2020-08-10 14:30:13 +0300

    fix

--- src/Concert.java
+++ src/Concert.java
@@ -19,10 +19,9 @@ public class Concert {
     private void makeShow(ArrayList<Human> list){
             for(Human human: list){
                 System.out.println(human.getName() + " что ты умеешь?");
-                System.out.println("Отвечает " + human.getName() + ": Все знают что " +
-                        human.getName() + " лучший " + human.skill + ".");
+                System.out.println("Отвечает " + human.getName() + ": Я лучший " + human.getTypeSkill() + ".");
                 System.out.println("Так сделай это!");
-                System.out.println(human.penName + ", " + human.skill + ". ");
+                System.out.println(human.getPenName() + ", " + human.getSkill() + ". ");
                 System.out.println(" ");
             }
         System.out.println("Всем спасибо за внимание!");
--- src/SelfEducation.java
+++
@@ -1,3 +0,0 @@
-public interface SelfEducation {
-    String getSelfEducationSkill();
-}
